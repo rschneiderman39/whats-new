0cd0cef5385ce8c1d923448294a5d89741d9a5bf
Rachel Schneiderman <rschneiderman379@gmail.com>
2018-12-03 21:04:40 -0800

    Starting to add in test frameworks and files. Removed release-notes-done (not sure what that was used for).

---
+++ spec/setup.spec.js
@@ -0,0 +1,11 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const setup = require('../setup');
+
+describe('Setup', () => {
+    
+});
